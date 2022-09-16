36bb3577f67c67bf2d870e1213152d6b56b81c8d
dulabdul <dul.abdoel21@gmail.com>
2022-09-16 18:28:01 +0700

    Refactor ClassComponent to Functional Component

--- src/components/SearchNotes.js
+++ src/components/SearchNotes.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { FaSearch } from 'react-icons/fa';
-function Search({ keyword, titleChange }) {
+function Search({ title, titleChange }) {
 	return (
 		<div className='notes-search'>
 			<FaSearch />{' '}
 			<input
 				type='text'
 				placeholder='Search Notes'
-				value={keyword}
+				value={title}
 				onChange={(e) => titleChange(e.target.value)}
 			/>
 		</div>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,34 +1,25 @@
-import React, { Component } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import Swal from 'sweetalert2';
 import NotesHiddenList from '../components/NotesHiddenList';
 import NotesShowList from '../components/NotesShowList';
 import Search from '../components/SearchNotes';
-import { getAllNotes } from '../utils/local-data';
+// import LocaleContext from '../contexts/LocaleContext';
+import { getAllNotes, deleteNote, archivedHandler } from '../utils/local-data';
 
-export default function HomePageWrapper() {
+export default function HomePage() {
 	const [searchParams, setSearchParams] = useSearchParams();
-	const title = searchParams.get('title');
-	function changeSearchParams(title) {
-		setSearchParams({ title });
-	}
-	return <HomePage defaulttitle={title} titleChange={changeSearchParams} />;
-}
-
-class HomePage extends Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			notes: getAllNotes(),
-			searchNotes: getAllNotes(),
-			title: props.defaulttitle || '',
-		};
-		this.onDeleteHandler = this.onDeleteHandler.bind(this);
-		this.onMoveArchiveHandler = this.onMoveArchiveHandler.bind(this);
-		this.onSearchHandler = this.onSearchHandler.bind(this);
-	}
-	onDeleteHandler(id) {
+	const [notes, setNotes] = useState(() => getAllNotes());
+	const [title, setTitles] = useState(() => {
+		return searchParams.get('title') || '';
+	});
+	// const {locale} = useContext(LocaleContext) kode untuk context nanti
+	// useEffect(() => {
+	// 	getAllNotes().then(({ data }) => {
+	// 		setNotes(data);
+	// 	});
+	// }, []);
+	const onDeleteHandler = (id) => {
 		Swal.fire({
 			title: 'Are you sure?',
 			text: 'Once deleted, you book totally removed!',
@@ -41,75 +32,52 @@ class HomePage extends Component {
 				Swal.fire('Poof! You notes success deleted', {
 					icon: 'success',
 				});
-				const notes = this.state.notes.filter((note) => note.id !== id);
-				const searchNotes = this.state.searchNotes.filter(
-					(note) => note.id !== id
-				);
-				this.setState({
-					notes,
-					searchNotes,
-				});
+				deleteNote(id);
+				setNotes(getAllNotes());
+				// const notes = this.state.notes.filter((note) => note.id !== id);
+				// const searchNotes = this.state.searchNotes.filter(
+				// 	(note) => note.id !== id
+				// );
+				// this.setState({
+				// 	notes,
+				// 	searchNotes,
+				// });
 			} else {
 				Swal.fire('Your notes file is cancel deleted!');
 			}
 		});
-	}
-	onMoveArchiveHandler(id) {
+	};
+	const onMoveArchiveHandler = (id) => {
 		Swal.fire({
 			title: 'Success',
 			type: 'success',
 			text: 'Your notes has been move.',
 		});
-		this.setState((prevState) => ({
-			notes: prevState.notes.map((note) => {
-				if (note.id === id) {
-					return {
-						...note,
-						archived: !note.archived,
-					};
-				}
-				return note;
-			}),
-			searchNotes: prevState.searchNotes.map((note) => {
-				if (note.id === id) {
-					return {
-						...note,
-						archived: !note.archived,
-					};
-				}
-				return note;
-			}),
-		}));
-	}
-	onSearchHandler(title) {
-		this.setState(() => {
-			return {
-				title,
-			};
-		});
-		this.props.titleChange(title);
-	}
-
-	render() {
-		const notes = this.state.notes.filter((note) => {
-			return note.title.toLowerCase().includes(this.state.title.toLowerCase());
-		});
-		return (
-			<>
-				<Search title={this.state.title} titleChange={this.onSearchHandler} />
-				<div className='container'>
-					<NotesShowList
-						notes={notes}
-						onDelete={this.onDeleteHandler}
-						onMoveArchive={this.onMoveArchiveHandler}
-					/>
-					<NotesHiddenList
-						notes={notes}
-						onDelete={this.onDeleteHandler}
-						onMoveArchive={this.onMoveArchiveHandler}
-					/>
-				</div>
-			</>
-		);
-	}
+		archivedHandler(id);
+		setNotes(getAllNotes());
+	};
+	const onSearchHandler = (title) => {
+		setTitles(title);
+		setSearchParams({ title });
+	};
+	const filteredNotes = notes.filter((note) => {
+		return note.title.toLowerCase().includes(title.toLowerCase());
+	});
+	return (
+		<>
+			<Search title={title} titleChange={onSearchHandler} />
+			<div className='container'>
+				<NotesShowList
+					notes={filteredNotes}
+					onDelete={onDeleteHandler}
+					onMoveArchive={onMoveArchiveHandler}
+				/>
+				<NotesHiddenList
+					notes={filteredNotes}
+					onDelete={onDeleteHandler}
+					onMoveArchive={onMoveArchiveHandler}
+				/>
+			</div>
+		</>
+	);
 }
