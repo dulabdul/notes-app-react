f5687757da354a279abbd6d8f427e554d826271b
dulabdul <dul.abdoel21@gmail.com>
2022-09-16 22:36:14 +0700

    Set Authed User Fitur

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import AddNotesPage from './pages/AddNotesPage';
 import DetailPage from './pages/DetailPage';
 import HomePage from './pages/HomePage';
 import NotFoundPages from './pages/NotFoundPage';
+import RegisterPage from './pages/RegisterPage';
 export default function App() {
-  return (
-    <>
-      <header>
-        <Header />
-      </header>
-      <main>
-        <Routes>
-          <Route exatch path='/' element={<HomePage />} />
-          <Route exatch path='/notes/:id' element={<DetailPage />} />
-          <Route exatch path='/new' element={<AddNotesPage />} />
-          <Route exatch path='*' element={<NotFoundPages />} />
-        </Routes>
-      </main>
-    </>
-  );
+	const [authedUser, setAuthedUser] = useState(null);
+	if (authedUser === null) {
+		return (
+			<main>
+				<Routes>
+					<Route path='/*' element={<p>Halaman Login</p>} />
+					<Route path='/register' element={<RegisterPage />} />
+				</Routes>
+			</main>
+		);
+	}
+	return (
+		<>
+			<header>
+				<Header />
+			</header>
+			<main>
+				<Routes>
+					<Route exatch path='/' element={<HomePage />} />
+					<Route exatch path='/notes/:id' element={<DetailPage />} />
+					<Route exatch path='/new' element={<AddNotesPage />} />
+					<Route exatch path='*' element={<NotFoundPages />} />
+				</Routes>
+			</main>
+		</>
+	);
 }
