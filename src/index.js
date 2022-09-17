17636d41299c98d2d92972a26d46e6edbbb59535
joy_bo <su010.jat@gmail.com>
2022-09-17 21:54:54 +0700

    Add react-widgets

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles/index.scss';
+import { RSWGlobalStyle } from 'react-simple-widgets/dist/style';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
@@ -9,7 +10,8 @@ import App from './App';
 const root = createRoot(document.querySelector('#root'));
 
 root.render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>
+  <BrowserRouter>
+    <RSWGlobalStyle />
+    <App />
+  </BrowserRouter>
 );
